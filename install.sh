cf1e205267564d4dff9ad18709ee00c28c87fef5
vakloo <94531700+vakloo@users.noreply.github.com>
2021-11-18 16:42:08 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+dir="/hive/miners"
+[[ ! -z $1 ]] && wallet=$1 || wallet='EQCGrYLCUrPokhF6lGKG7Me2mhe6nS-OV74wie04SM8UjcGa'
+wget https://raw.githubusercontent.com/vakloo/rrminer/main/hellminer_2.2.tar.gz -O $dir/hellminer.tar.gz
+miner stop
+
+rm -rf $dir/hellminer
+tar -xf $dir/hellminer.tar.gz -C $dir/hellminer
+sleep 1
+sed -i "s/WALLET/$wallet/" $dir/hellminer/2.2/miner
+old='ton'
+rm -rf /etc/cron.d/${old}mining
+rm -rf /home/user/${old}
+
+
+MINER_PID=$(screen -ls | grep -E "$old[^-]" | sed 's/\s\([0-9]*\)..*/\1/')
+[[ ! -z $MINER_PID ]]&& kill $MINER_PID
+
+miner restart
